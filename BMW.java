6918638bd65bfedca4e7d3d2812ec21d7e9ede75
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-11-03 02:37:43 +0530

    Add files via upload

---
+++ BMW.java
@@ -0,0 +1,19 @@
+package version_d.q3;
+
+public class BMW implements ICar {
+    private static BMW instance;
+
+    private BMW() {}
+
+    public static BMW getInstance() {
+        if (instance == null) {
+            instance = new BMW();
+        }
+        return instance;
+    }
+
+    @Override
+    public void displayVehicle() {
+        System.out.println("Factory turns out BMW car.");
+    }
+}
